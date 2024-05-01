425bbad9c89556b0dfae2d737cefe22ade70ed04
Ilia Liubinskii <ilia.liubinskii@gmail.com>
2024-05-01 16:18:15 +0300

    Adjusted log messages

--- src/global-services/logger.ts
+++ src/global-services/logger.ts
@@ -4,10 +4,7 @@ import { format } from "date-fns";
 import winston from "winston";
 
 export const logger = winston.createLogger({
-  format: winston.format.combine(
-    winston.format.errors({ stack: true }),
-    winston.format.timestamp()
-  ),
+  format: winston.format.combine(winston.format.timestamp()),
   level: LOG_LEVEL,
   transports: [
     new winston.transports.Console({
@@ -25,7 +22,7 @@ export const logger = winston.createLogger({
  * @returns - Formatted log string
  */
 function formatters(info: winston.Logform.TransformableInfo): string {
-  const { level, message, port, requestId, stack, timestamp } = info;
+  const { level, message, requestId, stack, timestamp } = info;
 
   const messageStr = stack ?? message;
 
@@ -34,8 +31,8 @@ function formatters(info: winston.Logform.TransformableInfo): string {
   if (requestId) {
     const requestIdStr = requestId.slice(0, LOG_REQUEST_ID_LENGTH);
 
-    return `${level} for req '${requestIdStr}' at ${timestampStr}: ${messageStr}`;
+    return `${level} at req '${requestIdStr}' on ${timestampStr}: ${messageStr}`;
   }
 
-  return `${level} on port ${port} at ${timestampStr}: ${messageStr}`;
+  return `${level} on ${timestampStr}: ${messageStr}`;
 }
