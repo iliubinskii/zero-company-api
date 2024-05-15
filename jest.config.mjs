3504995becec8a14c7f4adcc53146eb7d1f3a233
Ilia Liubinskii <ilia.liubinskii@gmail.com>
2024-05-15 21:41:20 +0300

    Chores

--- jest.config.mjs
+++ jest.config.mjs
@@ -6,6 +6,7 @@ const config = {
   collectCoverage: true,
   collectCoverageFrom: ["src/**/*.{js,jsx,ts,tsx}", "!**/*.d.ts"],
   coverageDirectory: "coverage",
+  coverageProvider: "v8",
   coverageReporters: ["lcov"],
   preset: "ts-jest",
   setupFilesAfterEnv: ["jest-extended/all", "./jest.setup.ts"],
