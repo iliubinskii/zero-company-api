b771a35cca2a54ddea565ab070de1bfc4adda9bc
Ilia Liubinskii <ilia.liubinskii@gmail.com>
2024-07-06 23:39:45 +0300

    chore: Replaced spell-checking eslint plugin

--- src/middleware/parse-form-data.ts
+++ src/middleware/parse-form-data.ts
@@ -2,14 +2,14 @@ import { MULTER_DESTINATION_PATH } from "../config";
 import type { RequestHandler } from "express";
 import multer from "multer";
 import path from "node:path";
-import { v4 as uuidv4 } from "uuid";
+import { v4 as uuid } from "uuid";
 
 const storage = multer.diskStorage({
   destination: (_req, _file, callback) => {
     callback(null, MULTER_DESTINATION_PATH);
   },
   filename: (_req, file, callback) => {
-    const basename = uuidv4();
+    const basename = uuid();
 
     const ext = path.extname(file.originalname);
 
